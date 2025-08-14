4dd9762a7d66c44946e897d4943889c84e54098c
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2025-08-14 15:15:34 +0000

    Removed t2 instance

--- vars.tf
+++ vars.tf
@@ -59,13 +59,12 @@ variable "instance_types" {
   type        = map(string)
   default = {
     asg_instance_type_1 = "t3.medium"
-    asg_instance_type_2 = "t2.medium"
-    asg_instance_type_3 = "c5.large"
-    asg_instance_type_4 = "c5d.large"
-    asg_instance_type_5 = "c6i.large"
-    asg_instance_type_6 = "c6id.large"
-    asg_instance_type_7 = "c7i.large"
-    asg_instance_type_8 = "c7i-flex.large"
+    asg_instance_type_2 = "c5.large"
+    asg_instance_type_3 = "c5d.large"
+    asg_instance_type_4 = "c6i.large"
+    asg_instance_type_5 = "c6id.large"
+    asg_instance_type_6 = "c7i.large"
+    asg_instance_type_7 = "c7i-flex.large"
   }
 }
 
