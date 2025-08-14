a4f2c37cf391d71311b66147288532e4fdffca06
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2025-08-14 15:15:47 +0000

    Output public ALB dns

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-# output "docker_swarm_dns_name" {
-#   value = var.create_extlb ? aws_lb.external_lb.*.dns_name : []
-# }
+output "docker_swarm_dns_name" {
+  value = var.create_extlb ? aws_lb.external_lb.*.dns_name : []
+}
 
 output "docker_swarm_manager_private_ips" {
   value = data.aws_instances.docker_swarm_managers.*.private_ips
